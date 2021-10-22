e0055d4c3fae1e16342f291aa9765b4265ea2f42
Sean <>
2021-10-22 14:01:35 -0700

    first

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Times from "../src/components/Times";
+import Count from "../src/components/Count";
+
+export default function Home() {
+  return (
+    <div>
+      <Times />
+      <Count />
+    </div>
+  );
+}
